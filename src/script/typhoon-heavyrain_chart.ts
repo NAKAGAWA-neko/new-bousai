4d371578640b231724d77a6880e72ff26620b2ab
NAKAGAWA-neko <goginzahiro@gmail.com>
2023-07-16 14:43:02 +0900

    修正

--- src/script/typhoon-heavyrain_chart.ts
+++ src/script/typhoon-heavyrain_chart.ts
@@ -4,19 +4,7 @@ import Chart, { ChartConfiguration } from "chart.js/auto";
 const rainRecordStChartConfig: ChartConfiguration<"bar", number[], string> = {
   type: "bar",
   data: {
-    labels: [
-      "2013",
-      "14",
-      "15",
-      "16",
-      "17",
-      "18",
-      "19",
-      "20",
-      "21",
-      "22",
-      "23",
-    ],
+    labels: ["2013", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23"],
     datasets: [
       {
         label: "",
@@ -79,23 +67,11 @@ const rainRecordStChartConfig: ChartConfiguration<"bar", number[], string> = {
   },
 };
 
-// 大雨特別警報
-const rainSplWarningChartConfig: ChartConfiguration<"bar", number[], string> = {
+// 大雨特別警報年別
+const rainSpWarningYeChartConfig: ChartConfiguration<"bar", number[], string> = {
   type: "bar",
   data: {
-    labels: [
-      "2013",
-      "14",
-      "15",
-      "16",
-      "17",
-      "18",
-      "19",
-      "20",
-      "21",
-      "22",
-      "23",
-    ],
+    labels: ["2013", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23"],
     datasets: [
       {
         label: "",
@@ -149,16 +125,72 @@ const rainSplWarningChartConfig: ChartConfiguration<"bar", number[], string> = {
     },
   },
 };
+// 大雨特別警報月別
+const rainSpWarningMoChartConfig: ChartConfiguration<"bar", number[], string> = {
+  type: "bar",
+  data: {
+    labels: ["7月", "8月", "9月", "10月"],
+    datasets: [
+      {
+        label: "",
+        data: [544, 178, 299, 472],
+        backgroundColor: ["rgba(232, 195, 52, 0.7)"],
+        borderColor: ["rgba(232, 195, 52, 1)"],
+        borderWidth: 1,
+      },
+    ],
+  },
+  options: {
+    plugins: {
+      legend: {
+        display: false,
+      },
+      tooltip: {
+        mode: "index",
+        filter: function (item) {
+          return item.raw !== 0; // '0'のデータポイントではツールチップを表示しない
+        },
+        callbacks: {
+          title: function (context) {
+            return context[0].label + "年";
+          },
+          label: function (context) {
+            var label = context.dataset.label || "";
+            if (label) {
+              label += ": ";
+            }
+            if (context.parsed.y !== null) {
+              label += context.parsed.y + "回";
+            }
+            return label;
+          },
+        },
+        bodyFont: {
+          weight: "bold", // テキストを太字に設定
+        },
+      },
+    },
+    scales: {
+      x: {
+        grid: {
+          display: false,
+        },
+        stacked: true,
+      },
+      y: {
+        stacked: true,
+      },
+    },
+  },
+};
 // ---------------------------------
 
 let rainRecordStChart: Chart | null = null;
-let rainSpWarningChart: Chart | null = null;
+let rainSpWarningYeChart: Chart | null = null;
+let rainSpWarningMoChart: Chart | null = null;
 
 // チャートを生成するための関数
-const generateChart = (
-  config: ChartConfiguration<"bar", number[], string>,
-  canvasId: string
-): Chart => {
+const generateChart = (config: ChartConfiguration<"bar", number[], string>, canvasId: string): Chart => {
   let canvas = document.getElementById(canvasId) as HTMLCanvasElement | null;
   if (!canvas) {
     throw new Error(`Cannot find a canvas element with id ${canvasId}`);
@@ -167,10 +199,7 @@ const generateChart = (
   return chart;
 };
 
-const generateLineChart = (
-  config: ChartConfiguration<"line", number[], string>,
-  canvasId: string
-): Chart => {
+const generateLineChart = (config: ChartConfiguration<"line", number[], string>, canvasId: string): Chart => {
   let canvas = document.getElementById(canvasId) as HTMLCanvasElement | null;
   if (!canvas) {
     throw new Error(`Cannot find a canvas element with id ${canvasId}`);
@@ -181,28 +210,26 @@ const generateLineChart = (
 
 // 初期チャートの生成
 
-rainSpWarningChart = generateChart(
-  rainSplWarningChartConfig,
-  "rainSpWarningChart"
-);
-
 rainRecordStChart = generateChart(rainRecordStChartConfig, "rainRecordStChart");
+
+rainSpWarningYeChart = generateChart(rainSpWarningYeChartConfig, "rainSpWarningYeChart");
+
+rainSpWarningMoChart = generateChart(rainSpWarningMoChartConfig, "rainSpWarningMoChart");
 
 // ウィンドウサイズの変更時にチャートを再描画
 window.addEventListener("resize", () => {
-  if (rainSpWarningChart) {
-    rainSpWarningChart.destroy();
-    rainSpWarningChart = generateChart(
-      rainSplWarningChartConfig,
-      "rainSpWarningChart"
-    );
-  }
-
   if (rainRecordStChart) {
     rainRecordStChart.destroy();
-    rainRecordStChart = generateChart(
-      rainRecordStChartConfig,
-      "rainRecordStChart"
-    );
+    rainRecordStChart = generateChart(rainRecordStChartConfig, "rainRecordStChart");
+  }
+
+  if (rainSpWarningYeChart) {
+    rainSpWarningYeChart.destroy();
+    rainSpWarningYeChart = generateChart(rainSpWarningYeChartConfig, "rainSpWarningYeChart");
+  }
+
+  if (rainSpWarningMoChart) {
+    rainSpWarningMoChart.destroy();
+    rainSpWarningMoChart = generateChart(rainSpWarningMoChartConfig, "rainSpWarningMoChart");
   }
 });
