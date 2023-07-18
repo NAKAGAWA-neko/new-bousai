7d14eb295ae18f4dbc61bd832527b68f67b5dfb5
NAKAGAWA-neko <goginzahiro@gmail.com>
2023-07-18 21:18:35 +0900

    修正

--- src/script/typhoon_heavyrain.ts
+++ src/script/typhoon_heavyrain.ts
@@ -1,3 +1,8 @@
+import jQuery from "jquery";
+const $ = jQuery;
+
+// -----------------------------
+
 // ページの全ての要素が読み込まれたら実行する
 window.onload = function () {
   // デフォルトで表示する要素を指定
@@ -36,3 +41,32 @@ window.onload = function () {
     });
   }
 };
+
+// --------------------------------------------
+
+//アコーディオンをクリックした時の動作
+$(".typhoon-item-title").on("click", function () {
+  //タイトル要素をクリックしたら
+  var findElm = $(this).next(".typhoon-item-box"); //直後のアコーディオンを行うエリアを取得し
+  $(findElm).slideToggle(); //アコーディオンの上下動作
+
+  if ($(this).hasClass("close")) {
+    //タイトル要素にクラス名closeがあれば
+    $(this).removeClass("close"); //クラス名を除去し
+  } else {
+    //それ以外は
+    $(this).addClass("close"); //クラス名closeを付与
+  }
+});
+
+//ページが読み込まれた際にopenクラスをつけ、openがついていたら開く動作※不必要なら下記全て削除
+$(window).on("load", function () {
+  // $(".typhoon-item").addClass("open"); //accordion-areaのはじめのliにあるsectionにopenクラスを追加
+  $(".open").each(function (_, element) {
+    //openクラスを取得
+    var Title = $(element).children(".typhoon-item-title"); //openクラスの子要素のtitleクラスを取得
+    $(Title).addClass("close"); //タイトルにクラス名closeを付与し
+    var Box = $(element).children(".typhoon-item-box"); //openクラスの子要素boxクラスを取得
+    $(Box).slideDown(500); //アコーディオンを開く
+  });
+});
